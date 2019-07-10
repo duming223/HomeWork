572db1a0fcbf784ac6fa582da2ff7e41d8b6451b
yz-meing <meing@LAPTOP-C96IJ8A8>
2019-07-10 17:10:48 +0800

    [练习]：自定义一个TageHelper

---
+++ CoreWeb/PagTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb
+{
+    //自定义一个TageHelper  
+    [HtmlTargetElement("pager",Attributes="pageInde,path")]
+    public class PagTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            object pageIndex = context.AllAttributes["pageIndex"].Value;
+            object path = context.AllAttributes["path"].Value;
+            output.Attributes.Add("href", $"{path}/page-{pageIndex}");
+
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -9,14 +9,7 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            for (int a = 1; a <= 9; a++)
-            {
-                for (int b = 1; b <= a; b++)
-                {
-                    Console.Write("{0}x{1}={2}\t", a, b, a * b); ;
-                }
-                Console.WriteLine();
-            }
+          
         }
     }
 
