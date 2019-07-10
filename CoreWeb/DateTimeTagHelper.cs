657c907dad36b254f57f43ec0feb7d75208390fc
yz-meing <meing@LAPTOP-C96IJ8A8>
2019-07-10 22:37:37 +0800

    [作业]：tagpage作业1 完成

---
+++ CoreWeb/DateTimeTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb
+{
+    [HtmlTargetElement("datetime")]
+    public class DateTimeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "small";
+
+            output.Content.SetHtmlContent($"{DateTime.Now.Year}年{DateTime.Now.Month}月{DateTime.Now.Day}日   {DateTime.Now.Hour}点{DateTime.Now.Minute}分");
+        }
+    }
+}
