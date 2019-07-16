c4c4ea3646bbd304991734b82d6593acfaf00a53
yz-meing <meing@LAPTOP-C96IJ8A8>
2019-07-16 14:33:07 +0800

    [练习]：创建一个 对象 下拉列表  验证码3部曲

--- CoreWeb/Pages/Index.cshtml.cs
+++ CoreWeb/Pages/Index.cshtml.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreWeb.Pages.Shared;
+using HomeWork._17bang;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CoreWeb.Pages
 {
     [BindProperties]
-    public class IndexModel :_LayoutModel
+    public class IndexModel : _LayoutModel
     {
-       
+        public Student Winner { get; set; }
+        public IEnumerable<SelectListItem> Students { get; set; }
+
         public void OnGet()
         {
-            
             Title = "首页！";
+
+            IList<Student> students= new List<Student>
+            {
+                new Student{ Id = 1,Name="xingren",Age=20},
+                new Student{ Id = 2,Name="duming",Age=20},
+                new Student{ Id = 3,Name="meinge",Age=20}
+            };
+
+            Students = new SelectList(students, "Id", "Name");
         }
+
+       
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
     }
 }
--- CoreWeb/Pages/Login.cshtml.cs
+++ CoreWeb/Pages/Login.cshtml.cs
@@ -3,29 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreWeb.Pages.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CoreWeb.Pages
 {
     [BindProperties]
-    public class LoginModel : PageModel
+    public class LoginModel :_LayoutModel
     {
-
-       [BindProperty(SupportsGet =true)]
+        [Required(ErrorMessage ="必须填写！")]
         public string UserName { get; set; }
-        
+
+        [Required(ErrorMessage = "必须填写！")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         
         public void OnGet()
         {
-            
+
         }
 
         public void OnPost()
         {
-            
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
         }
     }
 }
