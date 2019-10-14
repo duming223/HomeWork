13a2ce0ec80d81c4e4268bd60e8747e518ce243e
duming223 <meinge@foxmail.com>
2019-10-14 15:41:35 +0800

    git test

---
+++ CoreWeb/User.cs
@@ -0,0 +1,43 @@
+﻿using BLL.Repository;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BLL
+{
+    public class User:Entity
+    {
+        private const string _salt = "$*%";
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public int Integral { get; set; }
+        public Email Email { get; set; }
+
+        //private UserReporsitory _userReporsitory;
+
+        public User()
+        {
+            //_userReporsitory = new UserReporsitory();
+        }
+
+        public void Register()
+        {
+            PassWord = GetMd5Hash(PassWord);
+        }
+
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 mD5 = MD5.Create())
+            {
+                byte[] data = mD5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    stringBuilder.Append(data[i].ToString("x2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
